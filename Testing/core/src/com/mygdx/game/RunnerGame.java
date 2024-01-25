8284e3d90886f85fa48885d8bf6ddc70674ebd13
Iason iacovides <157727798+Ia50nas@users.noreply.github.com>
2024-01-25 17:26:09 +0100

    Add files via upload

---
+++ Testing/core/src/com/mygdx/game/RunnerGame.java
@@ -0,0 +1,23 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.Screens.PlayScreen;
+
+public class RunnerGame extends Game {
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+}
---
+++ Testing/core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -0,0 +1,54 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.RunnerGame;
+
+public class PlayScreen implements Screen{
+    private RunnerGame game;
+    Texture texture;
+    public PlayScreen(RunnerGame game){
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);           //clear screen
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
