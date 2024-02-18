ddce5747ad586c08f99c153086b9ca7d565c93f4
Iason iacovides <157727798+Ia50nas@users.noreply.github.com>
2024-02-18 18:54:41 +0100

    Movement

--- Testing/core/src/com/mygdx/game/Sprites/Runner.java
+++ Testing/core/src/com/mygdx/game/Sprites/Runner.java
@@ -1,19 +1,44 @@
 package com.mygdx.game.Sprites;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.RunnerGame;
 import com.mygdx.game.Screens.PlayScreen;
 
 public class Runner extends Sprite {
+    public enum State{FALLING, JUMPING,STANDING, RUNNING};
+    public  State CurrentState;
+    public  State PreviousState;
     public World world;
     public Body b2body;
     private TextureRegion RunnerStand;
+    private Animation RunnerRun;
+    private  Animation RunnerJump;
+    private float stateTimer;
+    private  boolean RunningRight;
     public Runner(World world, PlayScreen screen){
         super(screen.getAtlas().findRegion("Run_Right1"));
         this.world = world;
+        CurrentState = State.STANDING;
+        PreviousState = State.STANDING;
+        stateTimer = 0;
+        RunningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+            frames.add(new TextureRegion(getTexture(), 87, 16, 17,17));
+            frames.add(new TextureRegion(getTexture(), 106, 16, 17,17));
+            frames.add(new TextureRegion(getTexture(), 125, 16, 17,17));
+            frames.add(new TextureRegion(getTexture(), 144, 16, 17,17));
+        RunnerRun = new Animation(0.1f, frames);
+        frames.clear();
+
+
+        frames.add(new TextureRegion(getTexture(), 87, 16, 17,17));
+        RunnerJump = new Animation(0.1f, frames);
+
         defineRunner();
         RunnerStand = new TextureRegion(getTexture(), 87, 16, 17, 17);
         setBounds(0,0,48/RunnerGame.PPM,48/RunnerGame.PPM);
@@ -21,7 +46,53 @@ public class Runner extends Sprite {
     }
     public void update(float dt){
         setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight()/2);
+        setRegion(getFrame(dt));
     }
+
+    private TextureRegion getFrame(float dt) {
+        CurrentState = getState();
+
+        TextureRegion region;
+        switch (CurrentState){
+            case JUMPING:
+                region = (TextureRegion) RunnerJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = (TextureRegion) RunnerRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+                region = RunnerStand;
+                break;
+            default:
+                region = RunnerStand;
+                break;
+        }
+
+    if((b2body.getLinearVelocity().x < 0 || !RunningRight) && !region.isFlipX()){
+        region.flip(true, false);
+        RunningRight = false;
+    }
+    else if((b2body.getLinearVelocity().x > 0 || RunningRight) && region.isFlipX()){
+        region.flip(true, false);
+        RunningRight = true;
+    }
+    stateTimer = CurrentState == PreviousState ? stateTimer + dt : 0;
+    PreviousState = CurrentState;
+    return region;
+    }
+    public State getState(){
+        if(b2body.getLinearVelocity().y > 0)
+            return State.JUMPING;
+        else if(b2body.getLinearVelocity().y < 0 || (b2body.getLinearVelocity().y < 0 && PreviousState == State.JUMPING ))
+            return State.FALLING;
+        else if(b2body.getLinearVelocity().x != 0)
+            return  State.RUNNING;
+        else
+            return  State.STANDING;
+        }
+
+
     public  void defineRunner(){
         BodyDef bdef = new BodyDef();
         bdef.position.set(32 / RunnerGame.PPM, 32 / RunnerGame.PPM);
