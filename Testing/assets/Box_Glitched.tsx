7faddaa81acce456156031c0c229e7fcd135c1ef
Iason iacovides <157727798+Ia50nas@users.noreply.github.com>
2024-02-25 22:30:21 +0100

    update

--- Testing/assets/Box_Glitched.tsx
+++ Testing/assets/Box_Glitched.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Box_Glitched" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="Box_Glitched.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Box_Glitched" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Box_Glitched.png" width="128" height="128"/>
 </tileset>
