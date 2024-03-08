2f4cce1f1c82fb785f6f0eee8e2bf320aa249ffa
Iason iacovides <157727798+Ia50nas@users.noreply.github.com>
2024-03-08 14:28:30 +0100

    Info Screen fix

--- Testing/core/src/com/mygdx/game/Screens/InfoScreen.java
+++ Testing/core/src/com/mygdx/game/Screens/InfoScreen.java
@@ -25,7 +25,7 @@ public class InfoScreen implements Screen {
         this.screenManager = screenManager;
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        Bg = new TextureRegion(new Texture("art/Info_Screen.png"));
+        Bg = new TextureRegion(new Texture("Art/Info_Screen.png"));
 
         initButtons();
     }
